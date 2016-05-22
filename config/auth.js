42d43968b0fc80e7a6eea6ae220492dd1a4a7817
Ngoc Don <kaitoukid2701@gmail.com>
2016-05-23 05:11:53 +0700

    Fix callback URL

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'twitter': {
 		consumerKey: process.env.TWITTER_CONSUMER_KEY,
 		consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-		callbackURL: 'http://ngocdon0127.ddns.net/auth/twitter/callback',
+		callbackURL: 'http://fcc-pinterest.herokuapp.com/auth/twitter/callback',
 	}
 }
